8c7f8a1ea77187e4da2a94d0927b2cb8cbd1d816
Yansaro Rodriguez Paez <61998848+yarn-rp@users.noreply.github.com>
2022-01-15 18:55:40 -0500

    feat(Action-Sheets): initial setup
    missing doc

---
+++ lib/action_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ActionItem {
+  ActionItem(
+    this.text,
+    this.iconData,
+    this.onTap, {
+    this.isDefaultAction = false,
+    this.isDestructiveAction = false,
+  });
+
+  final IconData? iconData;
+  final String text;
+  final bool isDefaultAction;
+  final bool isDestructiveAction;
+  final VoidCallback onTap;
+}
